9c332a255343780dbf07c2eaac78b68234f862c0
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-03-13 15:22:14 +0600

    complete file setup for course registration.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='max-w-7xl mx-auto mt-12'>
+      <h1 className='text-3xl font-bold text-center text-[#1C1B1B] '>Course Registration</h1>
+    </div>
+  )
+}
+
+export default App
